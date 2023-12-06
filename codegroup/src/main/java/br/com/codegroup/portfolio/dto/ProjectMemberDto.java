bb22953cc6d1f89091eecce6ff7b483248e99367
João Robson <jreisam@gmail.com>
2023-12-06 11:58:23 -0400

    build ok

---
+++ codegroup/src/main/java/br/com/codegroup/portfolio/dto/ProjectMemberDto.java
@@ -0,0 +1,23 @@
+package br.com.codegroup.portfolio.dto;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import java.util.Date;
+
+/**
+ * @author João Robson 
+ */
+@Data
+public class ProjectMemberDto {
+
+    private Long idProjeto;
+    @Column(nullable = false)
+    private String nome;
+    private Date dataNascimento;
+    @Column(length = 14, unique = true, nullable = false)
+    private Long cpf;
+    private String cargo;
+    private boolean funcionario;
+
+}
---
+++ codegroup/src/main/java/br/com/codegroup/portfolio/repository/ProjectRepository.java
@@ -0,0 +1,27 @@
+package br.com.codegroup.portfolio.repository;
+
+import br.com.codegroup.portfolio.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author João Robson
+ */
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+
+    Optional<Project> findByNome(String nome);
+
+    Optional<Project> findById(Long id);
+
+
+
+    Optional<List<Project>> findAllByPessoas_Id(Long personId);
+}
